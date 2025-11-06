9e2b33b09f300cdade988658ba1652ac61cc4e61
bhavyaditpath <bhavyad.itpath@gmail.com>
2025-11-06 13:20:29 +0530

    Add app-details page

---
+++ app-details/src/main.single-spa.ts
@@ -0,0 +1,33 @@
+import { enableProdMode, NgZone } from '@angular/core';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+import { Router, NavigationStart } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (process.env['NODE_ENV'] === 'production') {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: (singleSpaProps) => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return bootstrapApplication(App, {
+      ...appConfig,
+      providers: [
+        ...(appConfig.providers || []),
+        ...getSingleSpaExtraProviders(),
+      ],
+    });
+  },
+  template: '<app-root />',
+  Router,
+  NavigationStart,
+  NgZone,
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
